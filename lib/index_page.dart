bfbd67ff5cc0c207851a5637c152447eafc7e182
nobuaki.kajiki <nobuaki.kajiki@avanade.com>
2022-06-21 11:26:59 +0900

    [add]transition_page

--- lib/index_page.dart
+++ lib/index_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:layout_training/hero_page.dart';
 import 'package:layout_training/responsive_design_page.dart';
 import 'package:layout_training/shrinkable_footer_page.dart';
+import 'package:layout_training/transition_page.dart';
 
 import './grid_view_page.dart';
 import 'sliver_app_bar_page.dart';
@@ -77,6 +78,19 @@ class IndexPage extends StatelessWidget {
                 }));
               },
               child: Text("animation_hero"),
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return TransitionPage();
+                }));
+              },
+              child: Text("animation_transition"),
             ),
           ),
         ],
